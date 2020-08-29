cfb1b3a5151d0b8397f181e8cbfc0714de3f1e99
Zeynep Yüksel <32435460+zeynepyuksel@users.noreply.github.com>
2020-08-29 13:51:58 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,6 +28,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final Color white = HexColor.fromHex('#FDFEFE');
+  final Color black = HexColor.fromHex('#000000');
+  final List<int> numbers = [1, 2, 3, 5, 8, 13, 21, 34, 55];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,21 +38,220 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: white,
         shadowColor: white,
         leading: IconButton(
+          disabledColor: black,
           icon: const Icon(Icons.arrow_back),
         ),
         actions: <Widget>[
           IconButton(
+            disabledColor: black,
             icon: const Icon(Icons.menu),
           ),
         ],
       ),
-      body: Center(
+      body: Container(
+        padding: new EdgeInsets.all(5.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            FirstPart(),
+            secondPart(),
+            Expanded(
+              child: Card(
+                color: Colors.white54,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                              padding:
+                                  const EdgeInsets.fromLTRB(15.0, 30.0, 0, 0),
+                              child: SizedBox(
+                                width: 50.0,
+                                height: 50.0,
+                                child: const Card(
+                                    color: Colors.red,
+                                    child: IconButton(
+                                      color: Colors.white,
+                                      disabledColor: Colors.white,
+                                      icon: const Icon(Icons.favorite),
+                                    )),
+                              )),
+                          Column(
+                            children: [
+                              Padding(
+                                padding:
+                                    const EdgeInsets.fromLTRB(15.0, 32.0, 0, 0),
+                                child: Text('Popular',
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold)),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.fromLTRB(10.0, 1.0, 0, 0),
+                                child: Text('Mango'),
+                              ),
+                            ],
+                          ),
+                          Spacer(),
+                          Padding(
+                            padding:
+                                const EdgeInsets.fromLTRB(10.0, 30.0, 0, 0),
+                            child: IconButton(
+                              color: Colors.black,
+                              disabledColor: Colors.black,
+                              icon: const Icon(Icons.arrow_forward),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    /*ListView.builder(
+                      shrinkWrap: true,
+                      itemBuilder: (ctx, int) {
+                        return Card(
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20.0),
+                                child: Image.asset(
+                                  'assets/images/cake.jpg',
+                                  fit: BoxFit.cover,
+                                )));
+                      },
+                    ),*/
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
+
+  Row FirstPart() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10.0, 8.0, 0, 0),
+                child: Text(
+                  'What do you want',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10.0, 0, 0, 8.0),
+                child: Text(
+                  'to eat today?',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          width: 50.0,
+          height: 50.0,
+          child: const Card(
+            child: IconButton(
+              color: Colors.black,
+              disabledColor: Colors.black,
+              icon: const Icon(Icons.search),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  SizedBox secondPart() => SizedBox(
+        height: 200,
+        child: ListView.builder(
+          itemCount: 10,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  child: Column(
+                    children: [
+                      Expanded(
+                          flex: 6,
+                          child: Container(
+                              child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  child: Image.asset(
+                                    'assets/images/cake.jpg',
+                                    fit: BoxFit.cover,
+                                  )))),
+                      Expanded(
+                          flex: 1,
+                          child: Row(
+                            children: [
+                              Text("      Cake - "),
+                              Text("10 items",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold)),
+                            ],
+                          )),
+                    ],
+                  ),
+                ));
+          },
+        ),
+      );
+  SizedBox thirdPart() => SizedBox(
+        child: ListView.builder(
+          itemCount: 10,
+          scrollDirection: Axis.vertical,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  child: Column(
+                    children: [
+                      Expanded(
+                          flex: 6,
+                          child: Container(
+                              child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  child: Image.asset(
+                                    'assets/images/cake.jpg',
+                                    fit: BoxFit.cover,
+                                  )))),
+                      Expanded(
+                          flex: 1,
+                          child: Row(
+                            children: [
+                              Text("      Cake - "),
+                              Text("10 items",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold)),
+                            ],
+                          )),
+                    ],
+                  ),
+                ));
+          },
+        ),
+      );
 }
 
 extension HexColor on Color {
